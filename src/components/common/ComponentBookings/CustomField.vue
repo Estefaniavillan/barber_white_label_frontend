8635b0ce78b1ecc33b26a9ba8b82f14fb7a5c8fc
Estefania Villan <evargasvi@cesde.net>
2023-11-19 16:42:55 -0500

    text

---
+++ src/components/common/ComponentBookings/CustomField.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(["title", "placeholder"])
+</script>
+
+<template>
+    <div>
+        <p class="text-center">{{ props.title }}</p>
+        <input type="text" :placeholder="props.placeholder"/>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
