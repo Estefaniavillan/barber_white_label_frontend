99ea471148290d78c7a3c0141f277814756d25df
Carlos10Maya <117688278+Carlos10Maya@users.noreply.github.com>
2023-11-18 19:50:40 -0500

    add: search field is created

---
+++ src/components/common/Search-field.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="buscador">
+      <input type="text" placeholder="Buscar producto">
+      <button type="button" class="btn btn-light"><img src="../../assets/img/icons/buscador.svg" alt="Buscar"> </button>
+    </div>
+  </template>
+  <style scoped>
+  .buscador{
+    background-color: #F5F7F9;
+    border-radius: 0.5rem;
+    padding: 0;
+  }
+  
+  input {
+    width: 40rem;
+    border: none;
+    outline: none;
+    background-color: #F5F7F9;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  </style>
